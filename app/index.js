f88a608a4fdadc0cc56e588999898f21dd60450a
Leo Colombaro <git@colombaro.fr>
2014-05-23 19:14:20 +0200

    Initialize Engine

---
+++ app/index.js
@@ -0,0 +1,129 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+var language = [
+  "english", "afrikaans", "ancientgreek", "arabic", "armenian", "assamese",
+  "basque", "bengali", "bokmal", "bulgarian", "catalan", "coptic", "croatian",
+  "czech", "danish", "dutch", "esperanto", "estonian", "farsi", "finnish",
+  "french", "galician", "german", "german-x-2013-05-26", "greek", "gujarati",
+  "hindi", "hungarian", "icelandic", "indonesian", "interlingua", "irish",
+  "italian", "kannada", "kurmanji", "latin", "latvian", "lithuanian",
+  "malayalam", "marathi", "mongolian", "mongolianlmc", "monogreek", "ngerman",
+  "ngerman-x-2013-05-26", "nynorsk", "oriya", "panjabi", "pinyin", "polish",
+  "portuguese", "romanian", "russian", "sanskrit", "serbian", "slovak",
+  "slovenian", "spanish", "swedish", "swissgerman", "tamil", "telugu",
+  "turkish", "turkmen", "ukenglish", "ukrainian", "uppersorbian",
+  "usenglishmax", "welsh"
+];
+
+var LatexGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.npmInstall();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+    var extensionName = this._.slugify(this.appname);
+
+    // Have Yeoman greet the user.
+    this.log(yosay('Welcome to the marvelous Latex generator!'));
+
+    var prompts = [{
+      name: 'projectName',
+      message: 'Project Name',
+      default: extensionName,
+      filter: function (input) {
+        return input.replace(/^yourls[\-_]?/, '').replace(/[\-_]?yourls$/, '');
+      }
+    },
+    {
+      name: 'projectDesc',
+      message: 'Project Description',
+      default: 'My new LaTex book.'
+    },
+    {
+      name: 'version',
+      message: 'Project version',
+      default: '0.0.1',
+      validate: function (input) {
+        return /^[0-9]\.[0-9](\.[0-9])?$/.test(input);
+      }
+    },
+    {
+      name: 'projectUrl',
+      message: 'Project homepage'
+    },
+    {
+      name: 'license',
+      message: 'License',
+      default: 'MIT'
+    },
+    {
+      name: 'authorName',
+      message: 'Author name',
+      default: this.user.git.username,
+      validate: function (input) {
+        return input ? true : false;
+      }
+    },
+    {
+      type: 'list',
+      choices: language,
+      name: 'language',
+      message: 'Choose your language for Babel',
+      default: 'english'
+    },
+    {
+      type: 'confirm',
+      name: 'bib',
+      message: 'Do you need a bibliography?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'gloss',
+      message: 'Do you need a glossary?',
+      default: false
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.projectName = props.projectName;
+      this.projectDesc = props.projectDesc;
+      this.version = props.version;
+      this.projectUrl = props.projectUrl;
+      this.license = props.license;
+      this.authorName = props.authorName;
+      this.language = props.language;
+      this.bib = props.bib;
+      this.gloss = props.gloss;
+
+      done();
+    }.bind(this));
+  },
+
+  app: function () {
+    this.mkdir('dist');
+    this.template('Gruntfile.js', 'Gruntfile.js');
+    this.template('main.tex', 'main.tex');
+    this.template('src/1.tex', 'src/1.tex');
+    if (this.bib) {
+      this.template('src/refs.bib', 'src/refs.bib');
+    }
+  },
+
+  projectfiles: function () {
+    this.template('package.json', 'package.json');
+    this.copy('editorconfig', '.editorconfig');
+  }
+});
+
+module.exports = LatexGenerator;
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,65 @@
+/*
+ * <%= projectName %>
+ * <%= projectUrl %>
+ * Licensed under <%= license %> License
+ */
+module.exports = function (grunt) {
+  'use strict';
+  // Project configuration
+  grunt.initConfig({
+    latex: {
+      pdf: {
+        src: 'main.tex',
+        options: {
+          outputDirectory: 'dist/',
+          jobname: '<%= projectName %>'
+        }
+      }<% if (bib) { %>,
+      bib: {
+        src: 'dist/<%= projectName %>.aux',
+        options: {
+          engine: 'bibtex',
+          interaction: false
+        }
+      }<% } %><% if (gloss) { %>,
+      gloss: {
+        src: 'dist/<%= projectName %>.aux',
+        options: {
+          engine: 'makeglossaries',
+          interaction: false
+        }
+      }<% } %>
+    },
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+        }
+      }
+    },
+    watch: {
+      latex: {
+        files: '**/*.tex',
+        tasks: ['latex', 'latex:pdf']
+      },<% if (bib) { %>
+      bibtex: {
+        files: '**/*.bib',
+        tasks: ['latex:bib', 'latex:pdf']
+      },<% } %>
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: ['dist/<%= projectName %>.pdf'],
+      },
+    }
+  });
+
+  // These plugins provide necessary tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-latex');
+
+  // Default task
+  grunt.registerTask('default', ['connect', 'watch']);
+};
