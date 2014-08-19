41b61bbf463b05fe563d89c0f6baf11f9dc7daed
Léo Colombaro <git@colombaro.fr>
2014-08-19 16:20:48 +0200

    Fixes tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 var assert = require('yeoman-generator').assert;
+var fs = require('fs-extra');
 
 describe('creates expected files', function () {
   before(function (done) {
@@ -10,7 +11,7 @@ describe('creates expected files', function () {
       .inDir(path.join(__dirname, './temp'))
       .withOptions({ 'skip-install': true })
       .withPrompt({
-        'name': 'Test LaTex',
+        'name': 'Test LaTeX',
         'class': 'book',
         'language': 'french',
         'bib': true
@@ -31,10 +32,32 @@ describe('creates expected files', function () {
     done();
   });
 
+  // Don't work, don't know why...
+  //it('latex:chapter generator', function (done) {
+  //  assert.file('src/1/main.tex');
+  //  assert.fileContent('main.tex', /\\input{src\/1\/main\.tex}/);
+  //  assert.fileContent('src/1/main.tex', /\\chapter{First Chapter}/);
+  //  done();
+  //});
+});
+
+describe('creates a chapter', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../chapter'))
+      .inDir(path.join(__dirname, './temp'), function (dir) {
+        fs.copySync(path.join(__dirname, '../app/templates'), dir)
+      })
+      .withPrompt({
+        'chapterName': 'Test LaTex',
+        'chapterNum': '2'
+      })
+      .on('end', done);
+  });
+
   it('latex:chapter generator', function (done) {
-    assert.file('src/1/main.tex');
-    assert.fileContent('main.tex', /\\input{src\/1\/main\.tex}/);
-    assert.fileContent('src/2/main.tex', /\\chapter{First Chapter}/);
+    assert.file('src/2/main.tex');
+    assert.fileContent('main.tex', /\\input{src\/2\/main\.tex}/);
+    assert.fileContent('src/2/main.tex', /\\chapter{Test LaTex}/);
     done();
   });
 });
