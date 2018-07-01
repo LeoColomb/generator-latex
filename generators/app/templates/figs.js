48018bbfd91507a41684bfa645649ca63d82c0f0
Léo Colombaro <git@colombaro.fr>
2018-07-01 16:26:37 +0200

    Lint and prettify

--- generators/app/templates/figs.js
+++ generators/app/templates/figs.js
@@ -6,8 +6,7 @@ require('shelljs/global');
 glob('**/*.svg', (err, files) => {
   files.forEach((file) => {
     const filename = file.replace('.svg', '');
-    const command = `inkscape --file="${filename}.svg"` +
-                    ` --export-pdf="${filename}.pdf"`;
+    const command = `inkscape --file="${filename}.svg" --export-pdf="${filename}.pdf"`;
 
     exec(command, (code, stdout, stderr) => {
       console.log(command);
